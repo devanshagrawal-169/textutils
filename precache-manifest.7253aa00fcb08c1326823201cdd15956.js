526cf0a8df6c38f48c7c25468ad497b14006924f
devanshagrawal-169 <devanshagrawal169@gmail.com>
2023-08-11 20:53:30 +0530

    Updates

---
+++ precache-manifest.7253aa00fcb08c1326823201cdd15956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6084086aef523cb6009",
+    "url": "/textutils/static/css/main.af469346.chunk.css"
+  },
+  {
+    "revision": "e6084086aef523cb6009",
+    "url": "/textutils/static/js/main.490590a5.chunk.js"
+  },
+  {
+    "revision": "9eff2318fe253be3dc05",
+    "url": "/textutils/static/js/runtime~main.43961880.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/textutils/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "4afbaaa46a6fbb5e409574f6150a9047",
+    "url": "/textutils/index.html"
+  }
+];
